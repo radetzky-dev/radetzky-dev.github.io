96bfa946b60b16d26ab53dbe71bbe663060a65b4
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-10 13:57:29 +0100

    converter

---
+++ lezione18/convertitore.js
@@ -0,0 +1,16 @@
+//convert  unità di misura (g/kg) e viceversa
+
+function converter(value, unit, targetUnit) {
+    const validValues = ["g", "Kg"];
+
+    //check se unit è riconosciuto g o Kg
+
+    //check se targetUnit è riconosciuto g o Kg
+
+    //check se value è numerico
+
+    //operare la conversione
+
+}
+
+const result = converter(1000, "g", "Kg");
