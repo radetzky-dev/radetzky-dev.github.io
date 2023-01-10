116850941d4a286710e762d8ef96caadbbbe1ebe
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-10 14:24:50 +0100

    convertotre ok

--- lezione18/convertitore.js
+++ lezione18/convertitore.js
@@ -1,16 +1,42 @@
-//convert  unità di misura (g/kg) e viceversa
-
+//Convertitore  unità di misura (g/kg) e viceversa
 function converter(value, unit, targetUnit) {
-    const validValues = ["g", "Kg"];
+  const validValues = ["g", "Kg"];
 
-    //check se unit è riconosciuto g o Kg
+  //check se unit è riconosciuto g o Kg
+  if (!validValues.includes(unit) || !validValues.includes(targetUnit)) {
+    //  throw new Error("Il convertitore accetta solo " + validValues.toString())
+    console.log(
+      "ATTENZIONE: Il convertitore accetta solo " + validValues.toString()
+    );
+    return 0;
+  }
 
-    //check se targetUnit è riconosciuto g o Kg
+  //check se value è numerico
+  if (typeof value !== "number") {
+    console.log("ATTENZIONE: Il convertitore accetta solo numeri");
+    return 0;
+  }
 
-    //check se value è numerico
+  console.log("Converto i valori...");
+  let x = 0;
+  if (unit === validValues[0] && targetUnit === validValues[1]) {
+    x = value / 1000;
+  } else if (unit === validValues[1] && targetUnit === validValues[0]) {
+    x = value * 1000;
+  }
 
-    //operare la conversione
-
+  return x;
 }
 
-const result = converter(1000, "g", "Kg");
+const showResult = function (result) {
+  if (result != 0) {
+    console.log("Risultato della conversione " + result);
+  } else {
+    console.log("Impossibile convertire");
+  }
+};
+
+showResult(converter(1500, "g", "Kg"));
+showResult(converter(1.4, "Kg", "g"));
+
+console.log("Operazione completata");
