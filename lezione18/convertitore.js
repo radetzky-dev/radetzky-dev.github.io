a1a59ce1075a324d1ef8376aed78e14f16fae121
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-11 09:47:46 +0100

    mocha test

--- lezione18/convertitore.js
+++ lezione18/convertitore.js
@@ -1,3 +1,5 @@
+var assert = require('assert');  //variabile per i test
+
 //Convertitore  unità di misura (g/kg) e viceversa
 function converter(value, unit, targetUnit) {
   const validValues = ["g", "Kg"];
@@ -36,7 +38,48 @@ const showResult = function (result) {
   }
 };
 
+/*
 showResult(converter(1500, "g", "Kg"));
 showResult(converter(1.4, "Kg", "g"));
+*/
+
+describe('converterTests', function () {
+  it('convert 1000g in 1Kg', function () {
+      const result = converter(1000, "g", "Kg");
+      assert.equal(result, 1);
+  });
+
+  it('convert 100g in 0.1Kg', function () {
+      const result = converter(100, "g", "Kg");
+      assert.equal(result, 0.1);
+  });
+
+  it('convert 1g in 0.001Kg', function () {
+      const result = converter(1, "g", "Kg");
+      assert.equal(result, 0.001);
+  });
+
+  it('convert 0g in 0Kg', function () {
+      const result = converter(0, "g", "Kg");
+      assert.equal(result, 0);
+  });
+
+  it('convert -1000g in 0Kg', function () {
+      const result = converter(-1000, "g", "Kg");
+      assert.equal(result, -1);
+  });
+
+  it('convert not a number throws', function () {
+      assert.throws(() => converter("not a number", "g", "Kg"), Error, "Il convertitore accetta solo numeri");
+  });
+
+  it('convert Kg to g', function () {
+      assert.equal(converter(1, "Kg", "g"), 1000);
+  });
+
+  it('convert something to blablabla', function () {
+      assert.throws(() => converter(1, "something", "blabla"), Error, "Il convertitore accetta: g,Kg");
+  });
+});
 
 console.log("Operazione completata");
