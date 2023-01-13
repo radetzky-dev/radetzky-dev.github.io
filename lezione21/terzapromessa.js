5ddb8a2decc44da1ae429108bf85be06a4cf8451
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-13 13:20:55 +0100

    parizale

--- lezione21/terzapromessa.js
+++ lezione21/terzapromessa.js
@@ -3,57 +3,28 @@ function checkFreeRoom() {
 }
 
 console.log("**** INIZIO PRENOTAZIONE STANZA ****");
-
-/*
-myPromise
-  .then((value) => `${value} and bar`)
-  .then((value) => `${value} and bar again`)
-  .then((value) => `${value} and again`)
-  .then((value) => `${value} and again`)
-  .then((value) => {
-    console.log(value);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-*/
-
 const promiseReservation = new Promise((resolve, reject) => {
   let result = checkFreeRoom();
-  console.log("Result " + result);
- 
-    console.log("ok");
+  console.log("Result: " + result);
+  if (result > 4) {
+    console.log("Stanza libera OK!");
     resolve("OK");
-
+  } else {
+    console.log("Nessuna stanza KO!");
+    reject("KO");
+  }
 });
 
-promiseReservation.then((successMessage) => {
-  console.log(`Bene: ${successMessage}`);
-});
-/*
-promiseReservation.catch((errorMessage) => {
-  console.error(`Peccato: ${errorMessage}`);
-});
+console.log("elaboro la tua prenotazione...");
 
-promiseReservation.finally((info) => console.log("Finito tutto"));*/
-console.log("**** FINITO PRENOTAZIONE STANZA ****");
-
-
-/*
-new Promise(tetheredGetNumber)
-  .then(determineParity, troubleWithGetNumber)
-  .then(promiseGetWord)
-  .then((info) => {
-    console.log(`Ho ottenuto: ${info.value}, ${info.wordEvenOdd}`);
-    return info;
+promiseReservation
+  .then((successMessage) => {
+    console.log(`Bene: ${successMessage}`);
   })
-  .catch((reason) => {
-    if (reason.cause) {
-      console.error("Had previously handled error");
-    } else {
-      console.error(`Trouble with promiseGetWord(): ${reason}`);
-    }
-  })
-  .finally((info) => console.log("Finito tutto"));
-*/
+  .catch((errorMessage) => {
+    console.error(`Peccato: ${errorMessage}`);
+  });
+
+promiseReservation.finally(() => {
+  console.log("**** FINITO PRENOTAZIONE STANZA ****");
+});
