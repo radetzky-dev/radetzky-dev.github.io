cd0db92c76d95802507be041d61b9e6b3da25365
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-17 10:29:41 +0100

    ts modificato

--- lezione23/altriesempi.ts
+++ lezione23/altriesempi.ts
@@ -2,8 +2,8 @@ let user: any = { userName: "Mario", userBirthYear: 1980 }; //recuperato da una 
 
 /**
  * Greet the user and show years
- * @param name 
- * @param age 
+ * @param name
+ * @param age
  */
 function greet(name: string, age: number): void {
   console.log("Hello, " + name.toUpperCase() + " hai " + age + " anni.");
@@ -11,11 +11,42 @@ function greet(name: string, age: number): void {
 
 /**
  * Get the age
- * @param birthYear 
- * @returns 
+ * @param birthYear
+ * @returns
  */
 function getAge(birthYear: number): number {
   return new Date().getFullYear() - birthYear;
 }
 
 greet(user["userName"], getAge(user["userBirthYear"]));
+
+const names: string[] = ["Alice", "Bob", "Eve"];
+
+// Contextual typing for function
+names.forEach(function (s) {
+  console.log(s);
+});
+
+/**
+ * Print coordinates
+ * @param pt
+ */
+function printCoord(pt: { x: number; y: number }) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
+
+/**
+ * printName and lastName (if provided : last is not mandatory)
+ * @param person 
+ */
+function printName(person: { first: string; last?: string }): void {
+  console.log("Name: " + person.first);
+  if (typeof person.last !== "undefined") {
+    console.log("Surname: " + person.last);
+  }
+}
+// Both OK
+printName({ first: "Bob" });
+printName({ first: "Alice", last: "Alisson" });
