d551b589fc3c492a7e0211450d89f9cb6176bc4e
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-19 12:00:56 +0100

    liste

--- lezione24/react/miaprima-app/src/index.js
+++ lezione24/react/miaprima-app/src/index.js
@@ -2,22 +2,50 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-function NumberList(props) {
-  //console.log(props);
-  const numbers = props.items;
-  const listItems = numbers.map((number) => (
-    <li key={number.toString()}>{number}</li>
-  ));
-  return <ul>{listItems}</ul>;
+
+function ingredientList(props) {
+  console.log(props.items);
+  return React.createElement(
+    "ul",
+    { className: "textclass" },
+    props.items.map((ingredient, i) =>
+      React.createElement("li", { key: i }, ingredient)
+    )
+  );
 }
 
-const items = [
-  "attività X",
-  "attività prova",
-  "attività ec",
-  "attività AAA",
-  "attività ZZZ",
+const myIngredients = [
+  "1 cup",
+  "1 crunh",
+  "1 egg",
+  "1 tea",
+  "1 milk",
+  "1 apple",
 ];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<NumberList items={items} />);
+const otherIngredients = [
+  "1 piatto",
+  "1 pane",
+  "1 farina",
+  "1 mandorle",
+];
+
+const lastIngredients = [
+  "1 mug",
+  "1 cavolo",
+  "1 miele",
+  "1 brodo",
+];
+
+function threeColumns() {
+  return React.createElement(
+    "div",
+    { className: "colonna" },
+    React.createElement(ingredientList, { items: myIngredients }, null),
+    React.createElement(ingredientList, { items: otherIngredients }, null),
+    React.createElement(ingredientList, { items: lastIngredients }, null)
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(React.createElement(threeColumns, null, null));
